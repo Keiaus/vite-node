85e5a1eb0089b4ecc040bf6976bdb01d6b81ae96
Keith Austin Jr <k.aus2029@gmail.com>
2023-08-19 07:03:10 -0400

    Trying to add .env to gitignore

--- vite-node-app/src/App.jsx
+++ vite-node-app/src/App.jsx
@@ -4,14 +4,16 @@ let App = () => {
 
   return (
     <>
-      <form action="">
-        <h1>Testing things out</h1>
-        <label htmlFor="fname">First Name: </label><br />
-        <input type="text" id='fname' name='fname'/><br />
-        <label htmlFor="lname">Last Name: </label><br />
-        <input type="text" id='lname' name='lname'/><br /><br />
-        <button type='submit'>Submit</button>
-      </form>
+      <div id='homepage'>
+        <form action="">
+          <h1>Testing things out</h1>
+          <label htmlFor="fname">First Name: </label><br />
+          <input type="text" id='fname' name='fname' /><br />
+          <label htmlFor="lname">Last Name: </label><br />
+          <input type="text" id='lname' name='lname' /><br /><br />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
     </>
   )
 }
