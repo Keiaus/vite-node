a5ec4307faf2976290f6df176e40e340ca8ba315
Keith Austin Jr <k.aus2029@gmail.com>
2023-09-10 22:15:32 -0400

    REST api stuff

--- vite-node-app/src/client/index.jsx
+++ vite-node-app/src/client/index.jsx
@@ -1,14 +1,35 @@
+import React, { useState } from "react";
 
 const Homepage = () => {
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+
+    const onSubmitForm = async(event) => {
+        event.preventDefault();
+        try {
+            const bodyOne = { firstName };
+            const bodyTwo = { lastName };
+            const response = await fetch("http://localhost:5222/users", {
+                method: "POST",
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify(bodyOne, bodyTwo)
+            });
+
+            console.log(response);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
     return (
         <>
             <div id='homepage'>
-                <form action="">
+                <form onSubmit={onSubmitForm}>
                     <h1>Testing things out</h1>
                     <label htmlFor="fname">First Name: </label><br />
-                    <input type="text" id='fname' name='fname' /><br />
+                    <input type="text" id='fname' name='fname' value={firstName}/><br />
                     <label htmlFor="lname">Last Name: </label><br />
-                    <input type="text" id='lname' name='lname' /><br /><br />
+                    <input type="text" id='lname' name='lname' value={lastName}/><br /><br />
                     <button type='submit'>Submit</button>
                 </form>
             </div>
